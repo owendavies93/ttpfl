14f7891e12030ef6733d872beb6de41ab19fc25f
Owen Davies <owen@odavi.es>
2024-12-30 08:50:21 +0000

    Rename config file

--- lib/TTPFL/Config.pm
+++ lib/TTPFL/Config.pm
@@ -7,7 +7,7 @@ use YAML::XS;
 our @EXPORT_OK = qw(get_weights);
 
 sub get_weights {
-    my $file = 'etc/weights.yaml';
+    my $file = 'etc/config.yaml';
     my $config = YAML::XS::LoadFile($file);
     return $config->{weights};
 }
